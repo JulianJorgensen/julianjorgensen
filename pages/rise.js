6cbef838b03d2fd9b5897dc065355c58f3d7b652
Julian Jorgensen <jj@julianjorgensen.com>
2019-01-06 11:45:20 +0100

    fix build errors

--- pages/rise.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import Page from 'components/Page';
-
-export default () => (
-  <Page isCase></Page>
-)
--- store/slideItems.js
+++ store/slideItems.js
@@ -1,32 +1,8 @@
-import goldenstrikeImage from 'assets/images/goldenstrike.jpg';
-
 export default [
   {
     slug: "",
     contentColor: "black",
     title: "Julian Jorgensen",
     subtitle: "I merge Frontend, UX and Branding. Delivering performant web apps and e-commerce sites.",
-  },
-  {
-    slug: "thankyou",
-    title: "Thank You",
-    subtitle: "Design. Experiences. Culture.",
-    background: "black",
-    contentColor: "white",
-  },
-  {
-    slug: "goldenstrike",
-    title: "Goldenstrike",
-    subtitle: "Frontend, UX, and project management",
-    background: "black",
-    contentColor: "white",
-    image: goldenstrikeImage
-  },
-  {
-    slug: "rise",
-    title: "RISE",
-    subtitle: "Frontend, UX, and project management",
-    background: "#ABE3FF",
-    contentColor: "white",
   },
 ];
